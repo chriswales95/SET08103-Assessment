65adc0599aa980f9e5e0cdb187bf2a08f63de66c
chris <chriswales95@outlook.com>
2019-03-30 20:31:08 +0000

    refactor database code

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -15,13 +15,12 @@ public class AppIntegrationTest
     static void init()
     {
         app = new App();
-        db = DatabaseHandler.Instance();
-        db.connect("localhost:33060");
+        app.connect("localhost:33060");
     }
 
     @Test
      void testReportOne(){
-        CountryReport r = (CountryReport) db.getReportOne();
+        CountryReport r = (CountryReport) app.getReportOne();
         int size =r.get_reportsItems().size();
         assertTrue(size >0);
 
@@ -29,14 +28,14 @@ public class AppIntegrationTest
 
     @Test
     void testReportTwo(){
-        CountryReport r = (CountryReport) db.getReportTwo("Africa");
+        CountryReport r = (CountryReport) app.getReportTwo("Africa");
         CountryReport.CountryReportItem i = r.get_reportsItems().get(0);
         assertEquals(111506000, i.get_population());
     }
 
     @Test
     void testReportThree(){
-        CountryReport r = (CountryReport) db.getReportThree("Western Africa");
+        CountryReport r = (CountryReport) app.getReportThree("Western Africa");
 
         CountryReport.CountryReportItem i = r.get_reportsItems().get(0);
         assertEquals("Nigeria", i.get_name());
@@ -44,7 +43,7 @@ public class AppIntegrationTest
 
     @Test
     void testReportFour(){
-        CountryReport r = (CountryReport) db.getReportFour(6);
+        CountryReport r = (CountryReport) app.getReportFour(6);
         CountryReport.CountryReportItem item = r.get_reportsItems().get(3);
 
         assertEquals("Jakarta", item.get_capital());
@@ -52,34 +51,34 @@ public class AppIntegrationTest
 
     @Test
     void testReportFive(){
-        CountryReport r = (CountryReport) db.getReportFive("Europe", 30);
+        CountryReport r = (CountryReport) app.getReportFive("Europe", 30);
         assertEquals(30, r.get_reportsItems().size());
     }
 
     @Test
     void testReportSix(){
-        CountryReport r = (CountryReport) db.getReportSix("Caribbean", 2);
+        CountryReport r = (CountryReport) app.getReportSix("Caribbean", 2);
         CountryReport.CountryReportItem item = r.get_reportsItems().get(1);
         assertEquals("Santo Domingo de Guzmán", item.get_capital());
     }
 
     @Test
     void testReportSeven() {
-        CityReport r = (CityReport) db.getReportSeven();
+        CityReport r = (CityReport) app.getReportSeven();
         CityReport.CityReportItem item = r.get_reportsItems().get(0);
         assertEquals(10500000, item.get_population());
     }
 
     @Test
     void testReportEight(){
-        CityReport r = (CityReport) db.getReportEight("Asia");
+        CityReport r = (CityReport) app.getReportEight("Asia");
         CityReport.CityReportItem i = r.get_reportsItems().get(0);
         assertEquals(10500000, i.get_population());
     }
 
     @Test
     void testReportNine(){
-        CityReport r = (CityReport) db.getReportNine("Western Europe");
+        CityReport r = (CityReport) app.getReportNine("Western Europe");
 
         CityReport.CityReportItem i = r.get_reportsItems().get(0);
         assertEquals("Berliini", i.get_district());
@@ -87,7 +86,7 @@ public class AppIntegrationTest
 
     @Test
     void testReportTen(){
-        CityReport r = (CityReport) db.getReportTen("france");
+        CityReport r = (CityReport) app.getReportTen("france");
 
         CityReport.CityReportItem i = r.get_reportsItems().get(0);
         assertEquals(2125246, i.get_population());
@@ -95,7 +94,7 @@ public class AppIntegrationTest
 
     @Test
     void testReportEleven(){
-        CityReport r = (CityReport) db.getReportEleven("Scotland");
+        CityReport r = (CityReport) app.getReportEleven("Scotland");
 
         CityReport.CityReportItem i = r.get_reportsItems().get(0);
         assertEquals(619680, i.get_population());
@@ -104,7 +103,7 @@ public class AppIntegrationTest
     @Test
     void testReportTwelve()
     {
-        CityReport r = (CityReport) db.getReportTwelve(8);
+        CityReport r = (CityReport) app.getReportTwelve(8);
 
         CityReport.CityReportItem i = r.get_reportsItems().get(0);
         assertEquals("Mumbai (Bombay)", i.get_name());
@@ -113,7 +112,7 @@ public class AppIntegrationTest
     @Test
     void testReportThirteen()
     {
-        CityReport r = (CityReport) db.getReportThirteen(5, "Europe");
+        CityReport r = (CityReport) app.getReportThirteen(5, "Europe");
 
         CityReport.CityReportItem i = r.get_reportsItems().get(0);
         assertEquals("Moscow", i.get_name());
@@ -122,7 +121,7 @@ public class AppIntegrationTest
     @Test
     void testReportFourteen()
     {
-        CityReport r = (CityReport) db.getReportFourteen(5, "Caribbean");
+        CityReport r = (CityReport) app.getReportFourteen(5, "Caribbean");
 
         CityReport.CityReportItem i = r.get_reportsItems().get(0);
         assertEquals("La Habana", i.get_name());
@@ -131,7 +130,7 @@ public class AppIntegrationTest
     @Test
     void testReportFifteen()
     {
-        CityReport r = (CityReport) db.getReportFifteen(5, "germany");
+        CityReport r = (CityReport) app.getReportFifteen(5, "germany");
 
         CityReport.CityReportItem i = r.get_reportsItems().get(0);
         assertEquals("Berlin", i.get_name());
@@ -140,7 +139,7 @@ public class AppIntegrationTest
     @Test
     void testReportSixteen()
     {
-        CityReport r = (CityReport) db.getReportSixteen(3, "Noord-Brabant");
+        CityReport r = (CityReport) app.getReportSixteen(3, "Noord-Brabant");
 
         CityReport.CityReportItem i = r.get_reportsItems().get(2);
         assertEquals("Netherlands", i.get_country());
@@ -149,7 +148,7 @@ public class AppIntegrationTest
     @Test
     void testReportSeventeen()
     {
-        CityReport r = (CityReport) db.getReportSeventeen();
+        CityReport r = (CityReport) app.getReportSeventeen();
 
         CityReport.CityReportItem i = r.get_reportsItems().get(2);
         assertEquals("Mexico", i.get_country());
@@ -158,7 +157,7 @@ public class AppIntegrationTest
     @Test
     void testReportEighteen()
     {
-        CityReport r = (CityReport) db.getReportEighteen("Europe");
+        CityReport r = (CityReport) app.getReportEighteen("Europe");
 
         CityReport.CityReportItem i = r.get_reportsItems().get(2);
         assertEquals("Germany", i.get_country());
@@ -167,7 +166,7 @@ public class AppIntegrationTest
     @Test
     void testReportNineteen()
     {
-        CityReport r = (CityReport) db.getReportNineteen("Caribbean");
+        CityReport r = (CityReport) app.getReportNineteen("Caribbean");
 
         CityReport.CityReportItem i = r.get_reportsItems().get(2);
         assertEquals("Haiti", i.get_country());
