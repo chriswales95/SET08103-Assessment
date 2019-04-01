ad03a3455f30fd5e9195af96254d66ddff883fcd
chris <chriswales95@outlook.com>
2019-04-01 14:12:26 +0100

    try this

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -27,7 +27,7 @@ public class App {
 
         // Connect to database
         if (args.length < 1) {
-            App.connect("localhost:33060");
+            App.connect("db:3306");
         } else {
             App.connect(args[0]);
         }
